f54cb7ad259fea577db68c131dcfa459fd7bdd54
khalilbouchnek <k.bouchnek@sciospec.de>
2026-03-12 15:15:46 +0100

    Update main_script.py
    Update ISX3 measurement setup

--- Python/ISX-3/src/main_script.py
+++ Python/ISX-3/src/main_script.py
@@ -3,16 +3,9 @@ from ISX3 import ISX3
 
 try:
     isx3 = ISX3(n_el=4)
-    isx3.connect_device_fs(port="COM3")  # change to com port if necessary
+    isx3.connect_device_fs(port="COM3")  # change to com port if necessary 
 
-    isx3.set_fs_settings(
-        measurement_mode=2,
-        measurement_channel="Main Port",
-        current_measurement_range="10mA",
-        voltage_measurement_range="autoranging"
-    )
-
-    isx3.set_setup(
+     isx3.set_setup(
         start_frequency="1kHz",
         end_frequency="10MHz",
         count=10,
@@ -22,9 +15,17 @@ try:
         excitation_type="voltage"
     )
 
+    isx3.set_fs_settings(
+        measurement_mode=2,
+        measurement_channel="Main Port",
+        current_measurement_range="10mA",
+        voltage_measurement_range="1V"
+    )
+
     results = isx3.start_measurement(spectra=2)
 
     print("Result Array: ", results)
 
 except AttributeError:
     print("Device is not connected.")
+
